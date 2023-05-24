bc8b05e862b96c1e40b1baf96becd506f0034396
Leonardo Santos <leonardojosantos@gmail.com>
2023-05-24 17:06:37 -0300

    Outros tipos de função.

---
+++ Modulo_01-Linguagem_funcional_no_Python/02-Outros_tipos_de_funcao/02-Funcoes_lambda/exemplo_01.py
@@ -0,0 +1,19 @@
+def multiplicar(a, b):
+    return a * b
+
+
+def main():
+    _a = 10
+    _b = 20
+
+    resultado = multiplicar(_a, _b)
+
+    print(f"\nA multiplicação entre {_a} e {_b} é: {resultado} \n")
+
+    # Função lambda
+    """ funcao_lambda = lambda a, b: 19 * 23
+        print(f"Função lambda = {funcao_lambda}") """
+
+
+if __name__ == "__main__":
+    main()
