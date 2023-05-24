ed8567982bb5f541d2c39b21f713b187a6961a0a
Leonardo Santos <leonardojosantos@gmail.com>
2023-05-24 17:28:07 -0300

    Atividade discursiva.

--- Modulo_01-Linguagem_funcional_no_Python/02-Outros_tipos_de_funcao/02-Funcoes_lambda/exemplo_01.py
+++ Modulo_01-Linguagem_funcional_no_Python/02-Outros_tipos_de_funcao/02-Funcoes_lambda/exemplo_01.py
@@ -1,5 +1,9 @@
-def multiplicar(a, b):
-    return a * b
+def multiplicar(_a, _b):
+    return _a * _b
+
+
+def multiplicar_lambda(_a, _b):
+    return lambda resultado: _a * _b
 
 
 def main():
@@ -7,12 +11,10 @@ def main():
     _b = 20
 
     resultado = multiplicar(_a, _b)
+    resultado_lambda = multiplicar_lambda(_a, _b)
 
-    print(f"\nA multiplicação entre {_a} e {_b} é: {resultado} \n")
-
-    # Função lambda
-    """ funcao_lambda = lambda a, b: 19 * 23
-        print(f"Função lambda = {funcao_lambda}") """
+    print(f"\nA multiplicação entre {_a} e {_b} é: {resultado}")
+    print(f"A multiplicação entre {_a} e {_b} é: {resultado_lambda} \n")
 
 
 if __name__ == "__main__":
---
+++ Modulo_01-Linguagem_funcional_no_Python/03-Atividade_discursiva/exercicio_01.py
@@ -0,0 +1,24 @@
+# script anonima.py (versão alterada do script funcao5.py)
+def multiplicar_por(multiplicador):
+    def mult(multiplicando):
+        return multiplicando * multiplicador
+    return mult
+
+
+def main():
+    multiplicador_10 = 10
+    multiplicador_5 = 5
+    multiplicando_1 = 1
+    multiplicando_2 = 2
+
+    multiplicar_por_10 = multiplicar_por(multiplicador_10)
+    print(multiplicar_por_10(multiplicando_1))
+    print(multiplicar_por_10(multiplicando_2))
+
+    multiplicar_por_5 = multiplicar_por(multiplicador_5)
+    print(multiplicar_por_5(multiplicando_1))
+    print(multiplicar_por_5(multiplicando_2))
+
+
+if __name__ == "__main__":
+    main()
